beb4a556a5d4826d010c429a751177d6808b31b7
Vitor Madeira <vitorsoutom@hotmail.com>
2026-04-24 10:59:12 -0300

    adiciona rota de atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,9 @@ app.post("/movies", async (req, res) => {
     });
 
     if (movieWithSameTitle) {
-      return res.status(409).send({ message: "Já existe um filme cadastrado com esse título" });
+      return res
+        .status(409)
+        .send({ message: "Já existe um filme cadastrado com esse título" });
     }
 
     await prisma.movie.create({
@@ -48,6 +50,38 @@ app.post("/movies", async (req, res) => {
   }
 
   res.status(201).send();
+});
+
+app.put("/movies/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  try {
+    const movie = await prisma.movie.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!movie) {
+      return res.status(404).send({ message: "Filme não encontrado" });
+    }
+
+    const data = { ...req.body };
+    data.release_date = data.release_date
+      ? new Date(data.release_date)
+      : undefined;
+
+    await prisma.movie.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  } catch (error) {
+    return res.status(500).send({ message: "Falha ao atualizar filme" });
+  }
+
+  res.status(200).send();
 });
 
 app.listen(port, () => {
